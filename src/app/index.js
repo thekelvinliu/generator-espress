286f2a359cf8b364e49c5cad116f8f1465bcccc9
thekelvinliu <kelvin@thekelvinliu.com>
2016-03-30 22:19:56 +0300

    added actual templates

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-// import path from 'path';
+import path from 'path';
 import { Base } from 'yeoman-generator';
+import pathExists from 'path-exists';
 import slug from 'slug';
 
 class MyGenerator extends Base {
@@ -10,32 +11,62 @@ class MyGenerator extends Base {
     slug.defaults.mode = 'rfc3986';
   }
 
-  get prompting() {
+  prompting() {
+    const done = this.async();
+    const prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'Enter the name of your project:',
+      default: 'name-of-your-project'
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project:'
+    }, {
+      type: 'input',
+      name: 'githubName',
+      message: 'Enter your github username:'
+    }, {
+      type: 'input',
+      name: 'buildDir',
+      message: 'Enter the name of your build directory:',
+      default: 'build'
+    }];
+    this.prompt(prompts, res => {
+      this.options.projectName = slug(res.projectName);
+      const dir = path.join('.', this.options.projectName);
+      if (!pathExists.sync(dir)) this.destinationRoot(dir);
+      else this.env.error(`${this.options.projectName} already exists.`);
+      this.options.description = res.description;
+      this.options.githubName = res.githubName;
+      this.options.buildDir = slug(res.buildDir);
+      done();
+    });
+  }
+
+  get writing() {
     return {
-      projectName() {
-        const done = this.async();
-        this.prompt({
-          type: 'input',
-          name: 'projectName',
-          message: 'Enter the name of your project:',
-          default: 'name-of-your-project'
-        }, res => {
-          this.options.projectName = slug(res.projectName);
-          done();
-        });
+      packageJson() {
+        this.fs.copyTpl(
+          this.templatePath('_package.json'),
+          this.destinationPath('package.json'),
+          {
+            projectName: this.options.projectName,
+            description: this.options.description,
+            githubName: this.options.githubName
+          }
+        );
       },
 
-      destDir() {
-        const done = this.async();
-        this.prompt({
-          type: 'input',
-          name: 'destDir',
-          message: 'Enter the name of your destination folder:',
-          default: 'build'
-        }, res => {
-          this.options.destDir = slug(res.destDir);
-          done();
-        });
+      bowerJson() {
+        this.fs.copyTpl(
+          this.templatePath('_bower.json'),
+          this.destinationPath('bower.json'),
+          {
+            projectName: this.options.projectName,
+            description: this.options.description
+          }
+        );
       }
     };
   }
