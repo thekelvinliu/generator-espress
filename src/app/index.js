ccd409c6238ddb7083a4cbe5e569764876bd2e49
thekelvinliu <kelvin@thekelvinliu.com>
2016-03-31 23:05:03 +0300

    finished git setup

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,28 @@
 
 import path from 'path';
 import { Base } from 'yeoman-generator';
+// import mkdirp from 'mkdirp';
 import pathExists from 'path-exists';
+import simpleGit from 'simple-git';
 import slug from 'slug';
 
 class MyGenerator extends Base {
   constructor(...args) {
     super(...args);
     slug.defaults.mode = 'rfc3986';
+    this.opts = {
+      year: new Date().getFullYear()
+    };
+    this.files = [
+      '.babelrc',
+      '.eslintrc.json',
+      '.gitignore',
+      'bower.json',
+      'gulpfile.babel.js',
+      'LICENSE',
+      'package.json',
+      'README.md'
+    ];
   }
 
   prompting() {
@@ -34,20 +49,22 @@ class MyGenerator extends Base {
       default: 'build'
     }];
     this.prompt(prompts, res => {
-      this.options.projectName = slug(res.projectName);
-      const dir = path.join('.', this.options.projectName);
+      this.opts.projectName = slug(res.projectName);
+      const dir = path.join('.', this.opts.projectName);
       if (!pathExists.sync(dir)) this.destinationRoot(dir);
-      else this.env.error(`the directory '${this.options.projectName}' already exists!`);
-      this.options.description = res.description;
-      this.options.githubName = res.githubName;
-      this.options.buildDir = slug(res.buildDir);
+      else this.env.error(`the directory '${this.opts.projectName}' already exists!`);
+      this.git = simpleGit(this.destinationRoot());
+      this.opts.description = res.description;
+      this.opts.githubName = res.githubName;
+      this.opts.buildDir = slug(res.buildDir);
       done();
     });
   }
 
   get writing() {
     return {
-      configs() {
+      // create root directory files
+      rootFiles() {
         this.fs.copy(
           this.templatePath('babelrc'),
           this.destinationPath('.babelrc')
@@ -57,38 +74,45 @@ class MyGenerator extends Base {
           this.destinationPath('.eslintrc.json')
         );
         this.fs.copyTpl(
-          this.templatePath('gitignore'),
-          this.destinationPath('.gitignore'),
-          {
-            buildDir: this.options.buildDir
-          }
-        );
-        this.fs.copyTpl(
           this.templatePath('_bower.json'),
           this.destinationPath('bower.json'),
-          {
-            projectName: this.options.projectName,
-            description: this.options.description
-          }
+          this.opts
         );
         this.fs.copyTpl(
           this.templatePath('_gulpfile.babel.js'),
           this.destinationPath('gulpfile.babel.js'),
-          {
-            buildDir: this.options.buildDir
-          }
+          this.opts
+        );
+        this.fs.copyTpl(
+          this.templatePath('_LICENSE'),
+          this.destinationPath('LICENSE'),
+          this.opts
         );
         this.fs.copyTpl(
           this.templatePath('_package.json'),
           this.destinationPath('package.json'),
-          {
-            projectName: this.options.projectName,
-            description: this.options.description,
-            githubName: this.options.githubName
-          }
+          this.opts
         );
       },
+      // express app config and files
       app() {
+      },
+      // create local git and commit all files
+      git() {
+        this.fs.copyTpl(
+          this.templatePath('gitignore'),
+          this.destinationPath('.gitignore'),
+          this.opts
+        );
+        this.fs.copyTpl(
+          this.templatePath('_README.md'),
+          this.destinationPath('README.md'),
+          this.opts
+        );
+        this.git
+          .init()
+          .add('.')
+          .commit(`${this.opts.projectName} initial commit`);
       }
     };
   }
@@ -98,7 +122,7 @@ class MyGenerator extends Base {
   }
 
   end() {
-    this.log(`'${this.options.projectName}' is all set up and ready to go!`);
+    this.log(`'${this.opts.projectName}' is all set up and ready to go!`);
   }
 }
 
